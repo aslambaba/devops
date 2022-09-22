9f88494e279e63179e9ee80bfd4305d57fbc61d0
aslambaba <aslam91r@gmail.com>
2022-09-22 05:40:58 +0500

    Minore tf changes

--- Projects/02_useTerraform_nginxLoadBalancer_andSetup_docker_instance/Iaac_main.tf
+++ Projects/02_useTerraform_nginxLoadBalancer_andSetup_docker_instance/Iaac_main.tf
@@ -212,13 +212,20 @@ resource "aws_instance" "Ansible_WorkerNode_3" {
   user_data = <<-EOL
     #!/bin/bash -xe
 
-    apt update
-    apt install apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-    apt install docker-ce
-    systemctl status docker
-    apt update
+    apt update 
+    apt-get remove docker docker-engine docker.io containerd runc -y
+    apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update -y
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
     EOL   
 }
 
